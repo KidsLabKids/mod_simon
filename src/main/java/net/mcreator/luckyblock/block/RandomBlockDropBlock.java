cfa8944d15de195d7484a06255e2e085be597a1f
kidslabkids <kidslab@KidsLabStation>
2021-12-16 15:51:53 +0100

    Slime eimer

---
+++ src/main/java/net/mcreator/luckyblock/block/RandomBlockDropBlock.java
@@ -0,0 +1,79 @@
+
+package net.mcreator.luckyblock.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.luckyblock.procedures.RandomProcedure;
+import net.mcreator.luckyblock.LuckyBlockModElements;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Collections;
+
+@LuckyBlockModElements.ModElement.Tag
+public class RandomBlockDropBlock extends LuckyBlockModElements.ModElement {
+	@ObjectHolder("lucky_block:random_block_drop")
+	public static final Block block = null;
+	public RandomBlockDropBlock(LuckyBlockModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.FOOD)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0)
+					.doesNotBlockMovement());
+			setRegistryName("random_block_drop");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+
+		@Override
+		public boolean removedByPlayer(BlockState blockstate, World world, BlockPos pos, PlayerEntity entity, boolean willHarvest, FluidState fluid) {
+			boolean retval = super.removedByPlayer(blockstate, world, pos, entity, willHarvest, fluid);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				RandomProcedure.executeProcedure($_dependencies);
+			}
+			return retval;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/luckyblock/procedures/RandomProcedure.java
@@ -0,0 +1,95 @@
+package net.mcreator.luckyblock.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.entity.projectile.FireballEntity;
+import net.minecraft.entity.passive.BeeEntity;
+import net.minecraft.entity.passive.BatEntity;
+import net.minecraft.entity.monster.CreeperEntity;
+import net.minecraft.entity.item.minecart.MinecartEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ILivingEntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.luckyblock.LuckyBlockMod;
+
+import java.util.Random;
+import java.util.Map;
+
+public class RandomProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				LuckyBlockMod.LOGGER.warn("Failed to load dependency x for procedure Random!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				LuckyBlockMod.LOGGER.warn("Failed to load dependency y for procedure Random!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				LuckyBlockMod.LOGGER.warn("Failed to load dependency z for procedure Random!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				LuckyBlockMod.LOGGER.warn("Failed to load dependency world for procedure Random!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		double Random = 0;
+		Random = (double) Math.random();
+		if ((Random < 0.5)) {
+			if (world instanceof ServerWorld) {
+				Entity entityToSpawn = new BatEntity(EntityType.BAT, (World) world);
+				entityToSpawn.setLocationAndAngles(x, y, z, world.getRandom().nextFloat() * 360F, 0);
+				if (entityToSpawn instanceof MobEntity)
+					((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+							SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+				world.addEntity(entityToSpawn);
+			}
+			if (world instanceof ServerWorld) {
+				Entity entityToSpawn = new BeeEntity(EntityType.BEE, (World) world);
+				entityToSpawn.setLocationAndAngles(x, y, z, world.getRandom().nextFloat() * 360F, 0);
+				if (entityToSpawn instanceof MobEntity)
+					((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+							SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+				world.addEntity(entityToSpawn);
+			}
+			if (world instanceof ServerWorld) {
+				Entity entityToSpawn = new CreeperEntity(EntityType.CREEPER, (World) world);
+				entityToSpawn.setLocationAndAngles(x, y, z, world.getRandom().nextFloat() * 360F, 0);
+				if (entityToSpawn instanceof MobEntity)
+					((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+							SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+				world.addEntity(entityToSpawn);
+			}
+		} else if (true) {
+			if (world instanceof ServerWorld) {
+				Entity entityToSpawn = new FireballEntity(EntityType.FIREBALL, (World) world);
+				entityToSpawn.setLocationAndAngles(x, y, z, world.getRandom().nextFloat() * 360F, 0);
+				if (entityToSpawn instanceof MobEntity)
+					((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+							SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+				world.addEntity(entityToSpawn);
+			}
+			if (world instanceof ServerWorld) {
+				Entity entityToSpawn = new MinecartEntity(EntityType.MINECART, (World) world);
+				entityToSpawn.setLocationAndAngles(x, y, z, world.getRandom().nextFloat() * 360F, 0);
+				if (entityToSpawn instanceof MobEntity)
+					((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+							SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+				world.addEntity(entityToSpawn);
+			}
+		}
+	}
+}
